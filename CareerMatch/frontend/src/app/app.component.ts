684a7aa72d6d1d5430eb1720b373d421baac9042
milos-nignite <milos.jakovljevic@nignite.com>
2026-01-21 14:16:43 +0100

    beautiful design

--- CareerMatch/frontend/src/app/app.component.ts
+++ CareerMatch/frontend/src/app/app.component.ts
@@ -1,7 +1,27 @@
 import { Component } from '@angular/core';
+import { AuthService } from './services/auth.service';
 
 @Component({
   selector: 'app-root',
-  template: `<router-outlet></router-outlet>`,
+  template: `
+  <div class="app-container">
+    <header class="header">
+      <div class="brand">
+        <div class="logo">CM</div>
+        <div>
+          <div class="title">CareerMatch</div>
+          <div class="muted">Find and post great jobs</div>
+        </div>
+      </div>
+      <div class="nav-right">
+        <div *ngIf="!auth.user" class="muted">Guest</div>
+        <div *ngIf="auth.user" class="muted">{{auth.user.email}}</div>
+        <button *ngIf="auth.user" class="button ghost" (click)="auth.logout()">Logout</button>
+      </div>
+    </header>
+
+    <router-outlet></router-outlet>
+  </div>
+  `
 })
-export class AppComponent {}
+export class AppComponent { constructor(public auth: AuthService){} }
